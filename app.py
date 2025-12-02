2801b6a13e9f0e89a2e1cc2c1ba420ae9260eefb
kimseohyeon20041105-arch <kimseohyeon20041105@gmail.com>
2025-12-03 03:45:31 +0900

    Update app.py

--- app.py
+++ app.py
@@ -193,7 +193,7 @@ if "recs" in st.session_state:
     # 피드백 입력
     st.markdown(
     """
-    <p style="font-size:20px; font-weight:600; text-align:center;">
+    <p style="font-size:30px; font-weight:600; ">
         <span style="color:#FF4B4B;">✍︎</span>
         <span style="color:#000000;">추천 피드백을 남겨주세요!</span>
     </p>
